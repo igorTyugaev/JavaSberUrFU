832f1bd1b804d23dd336c92b935bc14e87a2ecd3
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-10 17:40:52 +0500

    some refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,10 @@ import com.company.tasks.Task_1;
 import com.company.tasks.Task_2;
 import com.company.tasks.Task_3;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Task_1.testingTask();
         Task_2.testingTask();
         Task_3.testingTask();
-        ;
     }
 }
